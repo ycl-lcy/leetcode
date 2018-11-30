5591865d6ffafc404abe4f5d74edefcdc4a27ace
Ethan Liang <ethanl0518@gmail.com>
2018-11-30 19:54:41 +0800

    init

--- combination-sum/solve.py
+++ combination-sum/solve.py
@@ -1,45 +1,34 @@
 import itertools
 
-with open("input", "r") as f:
-    s = f.read()
-    s = s[:-1]
-i = s.find("[")+1
-p = int(s[s.find("]")+2])
-cand = []
-while(s[i] != "\n"):
-    cand.append(int(s[i]))
-    i += 2
-sols = []
-def solve(x, sol):
-    for ii,i in enumerate(cand):
-        sol.append(i)
-        x += i
-        if x < p:
-            x, sol = solve(x, sol)
-            x -= sol[-1]
-            sol.pop()
-            continue
-        if x >= p:
-            if x == p:
-                sols.append(sorted(sol))
-            x -= sol[-1]
-            sol.pop()
-            if ii == len(cand)-1:
-                return x, sol
-            continue
-    return x, sol
-solve(0, [])
-sols.sort()
-sols = list(sols for sols,_ in itertools.groupby(sols))
-print("[", end="")
-s = ""
-for sol in sols:
-    s += "["
-    for i in sol:
-        s += str(i)
-        s += ","
-    s = s[:-1]
-    s += "],"
-s = s[:-1]
-print(s, end="")
-print("]", end="")
+class Solution(object):
+
+    def combinationSum(self, candidates, target):
+        sols = []
+        self.solve(0, [], candidates, target, sols)
+        sols.sort()
+        sols = list(sols for sols,_ in itertools.groupby(sols))
+        return sols       
+    
+    def solve(self, x, sol, cand, t, sols):
+        for ii,i in enumerate(cand):
+            sol.append(i)
+            x += i
+            if x < t:
+                x, sol = self.solve(x, sol, cand, t, sols)
+                x -= sol[-1]
+                sol.pop()
+                continue
+            if x >= t:
+                if x == t:
+                    sols.append(sorted(sol))
+                x -= sol[-1]
+                sol.pop()
+                if ii == len(cand)-1:
+                    return x, sol
+                continue
+        return x, sol
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.combinationSum([8,7,4,3], 11)
+
